457c7ae30b536ef079bd243d0b417da34687ffd4
hancjozef <43301063+hancjozef@users.noreply.github.com>
2023-01-24 12:08:06 +0100

    Add files via upload

---
+++ labreport.py
@@ -0,0 +1,52 @@
+# nacitanie kniznic
+import numpy as np
+import pandas as pd
+import metrolopy as uc
+import sigfig as sf
+import warnings
+warnings.filterwarnings('ignore') # setting ignore as a parameter
+
+from numpy import array as v
+from numpy import float64 as dc
+from numpy import sqrt
+from scipy.stats import sem
+from numpy import mean
+from numpy import std as npstd
+from IPython.display import IFrame
+
+std = lambda x: npstd(x,ddof=1)
+
+uc.gummy.style = '+-'
+
+def budget(gvel, gnames, form = 'full', notation='decimal', transpose = True):
+    indirect = gnames[0]
+    direct = gnames[1:]
+    table = gvel[0].budget(gvel[1:], xnames = direct)
+    if form != 'final':
+        db = table.df.astype(float, errors='ignore')
+        db.set_index(['Component'], inplace=True)
+        db = db.reindex(direct+[indirect])
+        db.drop(columns='s', inplace=True)
+        db['vars'] = (db['u']*db['|dy/dx|'])**2
+        db.loc[indirect,'vars'] = db['vars'].sum() 
+        db.loc[indirect,'|dy/dx|'] = 1 
+        db['rel. vars %'] = db['vars']/db.loc[indirect,'u']**2*100
+        db['rel. u %'] = db['u']/db['Value']*100
+        db.set_index(['Unit'], append=True, inplace=True)
+        if notation == 'decimal':
+            table = db.fillna('').astype(str)
+        elif notation == 'sci':  
+            table = db.transpose().fillna('')
+        else:
+            table = db.fillna('')
+        if transpose:
+            table = table.transpose()
+    return table
+
+from googletrans import Translator
+translator = Translator()
+trENGSK = lambda text:translator.translate(text, src='en', dest='sk').text.replace('.','. ')
+
+from scipy.constants import find, physical_constants
+
+print('The contents of the package have been loaded successfully.')
